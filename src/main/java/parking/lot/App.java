a354e0113ba2d225fe0948336f5e17f513c82960
Vaibhav <vaibhavpandey92@gmail.com>
2018-01-01 02:07:26 +0530

    project setup

---
+++ src/main/java/parking/lot/App.java
@@ -0,0 +1,13 @@
+package parking.lot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
